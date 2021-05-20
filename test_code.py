5fb8b1ce2ea8bd556cdb066d521205f34bafd26e
Sadman Kabir Soumik <sadmanks@gmail.com>
2021-05-20 13:00:46 +0600

    readme updated

--- test_code.py
+++ test_code.py
@@ -1,10 +1,4 @@
-from collections import Counter
+from collections import namedtuple
 
-
-def most_frequent(lst):
-    data = Counter(lst)
-    return data.most_common(1)[0][0]  # [0][0] is the first element of a matrix
-
-
-list = [2, 1, 2, 2, 1, 3, 3, 3, 2]
-print(most_frequent(list))
+Fish = namedtuple("Fish", ["name", "species", "tank"])
+print(Fish)
